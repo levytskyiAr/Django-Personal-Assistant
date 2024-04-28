6e57e6d8cd95404cdc26c304a638cf9bd071715d
Artur <arturlevuckuy@gmail.com>
2024-04-28 21:21:06 +0200

    poprawky

--- DPA/notes/urls.py
+++ DPA/notes/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 
-app_name = 'app_notes'
-
 urlpatterns = [
     path('', views.main, name='main'),
     path('detail/<int:note_id>/', views.detail, name='detail'),
