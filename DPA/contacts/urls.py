4c72b270f33017dfc06776f04888975fb817ed48
mal4ishka <nesterovvlad@gmail.com>
2024-04-29 21:00:08 +0300

    contacts to profile

--- DPA/contacts/urls.py
+++ DPA/contacts/urls.py
@@ -5,5 +5,5 @@ app_name = 'contacts'
 
 urlpatterns = [
     # path('contacts/<str:user_id>/', views.get_user, name='author'),
-    path('contacts/', views.contact_list, name='contacts'),
+    path('profile/', views.contact_list, name='profile'),
 ]
