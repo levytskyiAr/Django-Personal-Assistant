0c115bc3d45dfea4713ec50b342f32fd5cb7fa6d
mal4ishka <nesterovvlad@gmail.com>
2024-04-29 20:47:56 +0300

    create postgres db, contacts base functionality

--- DPA/contacts/models.py
+++ DPA/contacts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(blank=True)
+    phone = models.CharField(max_length=10, blank=True)
+    birthday = models.DateField(null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='auth_user')
+
+    class Meta:
+        unique_together = ('first_name', 'last_name', 'user')
+        db_table = 'contacts'
