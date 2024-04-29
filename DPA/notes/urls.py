2dc026abf3ec11ace40869fdc5c7ef19ca8c0d85
Banch <andreypronko2002@gmail.com>
2024-04-29 23:29:34 +0200

    zaza

--- DPA/notes/urls.py
+++ DPA/notes/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'app_notes'
+app_name = 'notes'
 
 urlpatterns = [
     path('', views.main, name='main'),
@@ -11,5 +11,5 @@ urlpatterns = [
     path('done/<int:note_id>/', views.set_done, name='set_done'),
     path('delete/<int:note_id>/', views.delete_note, name='delete_note'),
     path('search_note/', views.search_note, name='search_note'),
-    path('app_notes/<int:note_id>/edit/', views.edit_note, name='edit_note'),
+    path('notes/<int:note_id>/edit/', views.edit_note, name='edit_note'),
 ]
