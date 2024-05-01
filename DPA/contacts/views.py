bcbfc9d21503be8b72b1aa3d4c4d600db6531d72
mal4ishka <nesterovvlad@gmail.com>
2024-05-01 16:21:31 +0300

    edit/delete contact

--- DPA/contacts/views.py
+++ DPA/contacts/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Contact
 from .forms import ContactForm
 from django.views.generic import ListView
+from django.views.generic import View
+from django.http import JsonResponse
 
 
 def contact_list(request):
@@ -38,3 +40,26 @@ class ContactListView(ListView):
 
     def get_queryset(self):
         return Contact.objects.filter(user=self.request.user)
+
+
+class EditContactView(View):
+    def get(self, request, contact_id):
+        contact = get_object_or_404(Contact, id=contact_id)
+        form = ContactForm(instance=contact)
+        return render(request, 'contacts/edit_contact.html', {'form': form, 'contact_id': contact_id})
+
+    def post(self, request, contact_id):
+        contact = get_object_or_404(Contact, id=contact_id)
+        form = ContactForm(request.POST, instance=contact)
+        if form.is_valid():
+            form.save()
+            return redirect('contacts:profile')
+        return render(request, 'contacts/edit_contact.html', {'form': form, 'contact_id': contact_id})
+
+
+def delete_contact(request, contact_id):
+    if request.method == 'POST':
+        contact = Contact.objects.get(id=contact_id)
+        contact.delete()
+        return redirect('contacts:profile')
+    return JsonResponse({'error': 'Invalid request method.'}, status=405)
