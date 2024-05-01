b41c7e57381313df32e5a266abcb2d6f77ab29e8
mal4ishka <nesterovvlad@gmail.com>
2024-05-02 01:13:06 +0300

    Contact list fix + phone format validation fix

--- DPA/contacts/forms.py
+++ DPA/contacts/forms.py
@@ -9,14 +9,14 @@ import re
 class ContactForm(forms.ModelForm):
 
     def validate_phone_number(value):
-        pattern = r'^\+380\d{2}-\d{3}-\d{2}-\d{2}$'
+        pattern = r'^0\d{2}-\d{3}-\d{2}-\d{2}$'
         if not re.match(pattern, value):
-            raise ValidationError('Invalid phone number format. Use format: +38099-999-99-99')
+            raise ValidationError('Invalid phone number format. Use format: 099-999-99-99')
 
     first_name = forms.CharField(label='First Name', widget=forms.TextInput(attrs={'placeholder': 'e.g. John'}))
     last_name = forms.CharField(label='Last Name', widget=forms.TextInput(attrs={'placeholder': 'e.g. Smith'}))
     email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'placeholder': 'name@example.com'}))
-    phone = forms.CharField(label='Phone', widget=forms.TextInput(attrs={'placeholder': '+38099-999-99-99'}), validators=[validate_phone_number])
+    phone = forms.CharField(label='Phone', widget=forms.TextInput(attrs={'placeholder': '099-999-99-99'}), validators=[validate_phone_number])
     birthday = forms.DateField(label='Birthday', widget=DateInput(attrs={'placeholder': 'yyyy-mm-dd'}))
 
     class Meta:
