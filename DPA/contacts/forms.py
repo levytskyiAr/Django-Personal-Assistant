840084228de2268ac6656680f0e3a6bff50e4799
mal4ishka <nesterovvlad@gmail.com>
2024-05-02 16:19:48 +0300

    upcoming_bdays functionality + general minor fixes + checks

--- DPA/contacts/forms.py
+++ DPA/contacts/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.forms.widgets import DateInput
 from .models import Contact
 from django.core.exceptions import ValidationError
-from django.utils.translation import gettext_lazy as _
 import re
 
 
@@ -15,13 +14,28 @@ class ContactForm(forms.ModelForm):
 
     first_name = forms.CharField(label='First Name', widget=forms.TextInput(attrs={'placeholder': 'e.g. John'}))
     last_name = forms.CharField(label='Last Name', widget=forms.TextInput(attrs={'placeholder': 'e.g. Smith'}))
-    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'placeholder': 'name@example.com'}))
-    phone = forms.CharField(label='Phone', widget=forms.TextInput(attrs={'placeholder': '099-999-99-99'}), validators=[validate_phone_number])
-    birthday = forms.DateField(label='Birthday', widget=DateInput(attrs={'placeholder': 'yyyy-mm-dd'}))
+    phone = forms.CharField(label='Phone', widget=forms.TextInput(attrs={'placeholder': 'Format: 099-999-99-99'}),
+                            validators=[validate_phone_number])
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'placeholder': 'Format: name@example.com'}))
+    address = forms.CharField(label='Address', widget=forms.TextInput(attrs={'placeholder': 'Type in your address'}))
+    birthday = forms.DateField(label='Birthday', widget=DateInput(attrs={'placeholder': 'Format: yyyy-mm-dd'}))
+
+    # def clean(self):
+    #     cleaned_data = super().clean()
+    #     first_name = cleaned_data.get('first_name')
+    #     last_name = cleaned_data.get('last_name')
+    #     phone = cleaned_data.get('phone')
+    #
+    #     if Contact.objects.filter(first_name=first_name, last_name=last_name, phone=phone,
+    #                               user_id=self.instance.user).exists():
+    #         raise forms.ValidationError(
+    #             "Contact with this name and phone number already exists for this user.")
+    #
+    #     return cleaned_data
 
     class Meta:
         model = Contact
-        fields = ['first_name', 'last_name', 'email', 'phone', 'birthday']
+        fields = ['first_name', 'last_name', 'phone', 'email', 'address', 'birthday']
 
 
 def validate_phone_number(value):
