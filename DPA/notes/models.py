6c6d8e2e036c4b1218e43b79c708d643e4b7efe8
Artur <arturlevuckuy@gmail.com>
2024-05-06 23:22:32 +0200

    перевірка

--- DPA/notes/models.py
+++ DPA/notes/models.py
@@ -5,7 +5,7 @@ User = get_user_model()
 
 class Note(models.Model):
     title = models.CharField(max_length=50, null=False)
-    note = models.CharField(null=False)
+    note = models.CharField(max_length=100000, null=False)
     tags = models.CharField(max_length=20, null=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
 
