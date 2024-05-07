638373253e0082459092a991cb74c29caf747e42
mal4ishka <nesterovvlad@gmail.com>
2024-05-07 20:13:31 +0300

    pass restore

--- DPA/DPA/settings.py
+++ DPA/DPA/settings.py
@@ -174,3 +174,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRONJOBS = [
     ('*/6 * * * *', 'myapp.cron.get_access_token')  # Запуск кожні 6 днів
 ]
+
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_STARTTLS = os.getenv('EMAIL_STARTTLS')
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+PASSWORD_RESET_CONFIRM_URL = 'password_reset_confirm'
