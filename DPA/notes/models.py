cc585ee136bbf9c63fc20de403782e924b425b17
Bancheer <andreypronko2002@gmail.com>
2024-05-08 20:21:30 +0200

    banch

--- DPA/notes/models.py
+++ DPA/notes/models.py
@@ -4,9 +4,9 @@ from django.db import models
 User = get_user_model()
 
 class Note(models.Model):
-    title = models.CharField(max_length=50, null=False)
+    title = models.CharField(max_length=50, blank=True)
     note = models.CharField(null=False)
-    tags = models.CharField(max_length=20, null=False)
+    tags = models.CharField(max_length=20, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
 
     class Meta:
